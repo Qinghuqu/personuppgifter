eaf3beaf352e6144ab5564ad1cf3daaf29fdaa58
qinghuqu <qinghuqu@gmail.com>
2022-03-01 16:00:41 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var titleSphere = document.querySelector('#titleSphere');
+var yPos = window.scrollY;
+
+console.log(yPos);
+
+for (;;)
+{
+  if (1000 < yPos)
+  {
+    alert("yPos bigger than 1000");
+    var timeout = setTimeout()
+  }
+}
