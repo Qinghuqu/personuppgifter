48f25027d04885869341f23796e863b7610c024b
qinghuqu <qinghuqu@gmail.com>
2022-04-19 15:59:44 +0200

    Writing content

--- script.js
+++ script.js
@@ -9,8 +9,79 @@ let answers =
 [
   {
     question: 'Vad är GDPR? Vilket är syftet med GDPR?',
-    answerText: 'Dataskyddsförordningen (GDPR) är en europeisk förordning som avser hur personliga uppgifter får hanteras och spridas. Ett av dess syften enligt Integritetsskyddsmyndigheten (imy.se): "Att skydda enskildas grundläggande rättigheter och friheter, särskilt deras rätt till skydd av personuppgifter." Privatliv är en rätt som medborgarna har rätt till, '
+    answerText: 'Dataskyddsförordningen (GDPR) är en europeisk förordning som avser hur personliga uppgifter får hanteras och spridas. Ett av dess syften enligt Integritetsskyddsmyndigheten (imy.se): "Att skydda enskildas grundläggande rättigheter och friheter, särskilt deras rätt till skydd av personuppgifter."'
+  },
+
+  {
+    question: 'Vad räknas som personuppgifter?',
+    answerText: 'Personuppgifter är information som kan kopplas till en individ. Dessa kan vara namn, födelsedatum, bostadsadress, kontaktuppgifter, jobb med mera.'
+  },
+
+  {
+    question: 'Vad kan känsliga personuppgifter vara?',
+    answerText: 'Några exempel på känsliga personuppgifter är medicinsk information och brottsregister.'
+  },
+
+  {
+    question: 'Vilka personuppgifter kan du publicera på din hemsida?',
+    answerText: ''
+  },
+
+  {
+    question: 'Vad är det för skillnad på Tryckfrihetsförordningen (TF) och Yttrandefrihetsgrundlagen (YGL)?',
+    answerText: ''
+  },
+
+  {
+    question: 'Vad går Tryckfrihetsförordningen (TF) och Yttrandefrihetsgrundlagen (YGL) ut på?',
+    answerText: ''
+  },
+
+  {
+    question: 'Vilka undantag finns från Tryckfrihetsförordningen (TF) och Yttrandefrihetsgrundlagen (YGL)?',
+    answerText: ''
+  },
+
+  {
+    question: 'Ett av undantagen har en koppling till känsliga personuppgifter? Vilken?',
+    answerText: ''
+  },
+
+  {
+    question: 'Berätta om den ansvarige utgivarens roll då det gäller Tryckfrihetsförordningen (TF) och Yttrandefrihetsgrundlagen (YGL).',
+    answerText: ''
+  },
+
+  {
+    question: 'Vad innebär upphovsrätt? Vad omfattas av upphovsrätten?',
+    answerText: ''
+  },
+
+  {
+    question: 'Hur länge gäller upphovsrätten?',
+    answerText: ''
+  },
+
+  {
+    question: 'Får du visa vilken bild som helst på din hemsida?',
+    answerText: ''
+  },
+
+  {
+    question: 'Får du möjliggöra spelandet av en låt på din hemsida?',
+    answerText: ''
+  },
+
+  {
+    question: 'Får du möjliggöra spelandet av en film på din hemsida?',
+    answerText: ''
+  },
+
+  {
+    question: 'Vad är Creative Commons? Hur kan du ha nytta av det som webbutvecklare?',
+    answerText: ''
   }
+
 ];
 
 
@@ -23,9 +94,15 @@ let answer = document.querySelector('#answer');
 let lastAnswer = document.querySelector('#lastAnswer');
 let nextAnswer = document.querySelector('#nextAnswer');
 
-answer.innerHTML = "<h1 class='question'>" + answers[0].question + "</h1><p class='answer-text'>" + answers[0].answerText + "</p>";
-lastAnswer.innerHTML = "answer" + String(yPosFloor - 1);
-nextAnswer.innerHTML = "answer" + String(yPosFloor + 1);
+answer.innerHTML = "<h1 class='question'>" + answers[yPosFloor].question + "</h1><p class='answer-text'>" + answers[yPosFloor].answerText + "</p>";
+if (yPosFloor >= 1)
+{
+  lastAnswer.innerHTML = "<h1 class='question'>" + answers[yPosFloor - 1].question + "</h1><p class='answer-text'>" + answers[yPosFloor - 1].answerText + "</p>";
+}
+if (yPosFloor < answers.length - 1)
+{
+  nextAnswer.innerHTML = "<h1 class='question'>" + answers[yPosFloor + 1].question + "</h1><p class='answer-text'>" + answers[yPosFloor + 1].answerText + "</p>";
+}
 
 
 console.log(titles);
@@ -57,10 +134,6 @@ for (let j = -3; j <= 3; j++)
   catch(e){}
 }
 titles[yPosFloor].style.fontWeight = "bold";
-
-
-let timeout1, timeout2, timeout3;
-let isRotating = false;
 
 
 function loop(n)
@@ -95,9 +168,15 @@ function loop(n)
       document.body.removeChild(lastAnswer);
       document.body.appendChild(lastAnswer);
 
-      answer.innerHTML = "answer" + String(yPosFloor);
-      nextAnswer.innerHTML = "answer" + String(yPosFloor + 1);
-      lastAnswer.innerHTML = "answer" + String(yPosFloor - 1);
+      answer.innerHTML = "<h1 class='question'>" + answers[yPosFloor].question + "</h1><p class='answer-text'>" + answers[yPosFloor].answerText + "</p>";
+      if (yPosFloor >= 1)
+      {
+        lastAnswer.innerHTML = "<h1 class='question'>" + answers[yPosFloor - 1].question + "</h1><p class='answer-text'>" + answers[yPosFloor - 1].answerText + "</p>";
+      }
+      if (yPosFloor < answers.length - 1)
+      {
+        nextAnswer.innerHTML = "<h1 class='question'>" + answers[yPosFloor + 1].question + "</h1><p class='answer-text'>" + answers[yPosFloor + 1].answerText + "</p>";
+      }
 
       answer.style = "animation: answer-change-down 1s;";
       lastAnswer.style = "animation: last-answer-change-down 1s;";
@@ -110,9 +189,15 @@ function loop(n)
       document.body.removeChild(nextAnswer);
       document.body.appendChild(nextAnswer);
 
-      answer.innerHTML = "answer" + String(yPosFloor);
-      nextAnswer.innerHTML = "answer" + String(yPosFloor + 1);
-      lastAnswer.innerHTML = "answer" + String(yPosFloor - 1);
+      answer.innerHTML = "<h1 class='question'>" + answers[yPosFloor].question + "</h1><p class='answer-text'>" + answers[yPosFloor].answerText + "</p>";
+      if (yPosFloor >= 1)
+      {
+        lastAnswer.innerHTML = "<h1 class='question'>" + answers[yPosFloor - 1].question + "</h1><p class='answer-text'>" + answers[yPosFloor - 1].answerText + "</p>";
+      }
+      if (yPosFloor < answers.length - 1)
+      {
+        nextAnswer.innerHTML = "<h1 class='question'>" + answers[yPosFloor + 1].question + "</h1><p class='answer-text'>" + answers[yPosFloor + 1].answerText + "</p>";
+      }
 
       answer.style = "animation: answer-change-up 1s;";
       nextAnswer.style = "animation: next-answer-change-up 1s;";
